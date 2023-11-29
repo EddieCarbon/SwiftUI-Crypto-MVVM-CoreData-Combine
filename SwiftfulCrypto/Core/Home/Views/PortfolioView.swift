2675d5a077292c3c4dd03ab80f89973d947b9581
Jakub Tomiczek <tomiczekkuba01@gmail.com>
2023-11-29 19:03:29 +0100

    Add PortfolioView

---
+++ SwiftfulCrypto/Core/Home/Views/PortfolioView.swift
@@ -0,0 +1,18 @@
+//
+//  PortfolioView.swift
+//  SwiftfulCrypto
+//
+//  Created by Jakub Tomiczek on 28/11/2023.
+//
+
+import SwiftUI
+
+struct PortfolioView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PortfolioView()
+}
